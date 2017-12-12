a35ee13362b0048ce439eeee067230502865e718
farisj <jakefaris@outlook.com>
2017-12-11 21:18:13 -0500

    Remove comments and unused methods

--- bots.rb
+++ bots.rb
@@ -6,58 +6,20 @@ require 'twitter_ebooks'
 class MyBot < Ebooks::Bot
   # Configuration here applies to all MyBots
   def configure
-    # Consumer details come from registering an app at https://dev.twitter.com/
-    # Once you have consumer details, use "ebooks auth" for new access tokens
     self.consumer_key = ENV["consumer_key"] # Your app consumer key
     self.consumer_secret = ENV["consumer_secret"] # Your app consumer secret
-
-    # Users to block instead of interacting with
-    #self.blacklist = ['tnietzschequote']
-
-    # Range in seconds to randomize delay when bot.delay is called
     self.delay_range = 1..6
   end
 
   def on_startup
     @model = Ebooks::Model.load('model/mine.model')
     scheduler.every '2h' do
-      # Tweet something every 24 hours
-      # See https://github.com/jmettraux/rufus-scheduler
-      # tweet("hi")
-      # pictweet("hi", "cuteselfie.jpg")
       tweet(@model.make_statement(140))
     end
   end
 
-  def on_message(dm)
-    # Reply to a DM
-    # reply(dm, "secret secrets")
-  end
-
-  def on_follow(user)
-    # Follow a user back
-    # follow(user.screen_name)
-  end
-
   def on_mention(tweet)
-    # Reply to a mention
-    # reply(tweet, "oh hullo")
     reply(tweet, @model.make_statement(140))
-  end
-
-  def on_timeline(tweet)
-    # Reply to a tweet in the bot's timeline
-    # reply(tweet, "nice tweet")
-  end
-
-  def on_favorite(user, tweet)
-    # Follow user who just favorited bot's tweet
-    # follow(user.screen_name)
-  end
-
-  def on_retweet(tweet)
-    # Follow user who just retweeted bot's tweet
-    # follow(tweet.user.screen_name)
   end
 end
 
